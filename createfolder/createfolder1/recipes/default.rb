e47f170cb17473d7117a515d6d647cab4b92af32
Luke Luo <lukel@realnetworks.com>
2016-11-07 10:52:27 -0500

    add new recipe

---
+++ createfolder/createfolder1/recipes/default.rb
@@ -0,0 +1,15 @@
+directory "/srv/www1/shared" do
+  mode 0755
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+end
+
+cookbook_file "/srv/www1/shared/example_data.json" do
+  source "example_data.json"
+  mode 0644
+  action :create_if_missing
+end
+
+
