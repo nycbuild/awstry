ffdf8c0c460df82e90051e09ea51a11de2bbc634
Luke Luo <lukel@realnetworks.com>
2016-11-16 11:45:45 -0500

    update script ownership

--- deployment-scripts/recipes/default.rb
+++ deployment-scripts/recipes/default.rb
@@ -1,5 +1,5 @@
 directory "/home/realntwk/deployment-scripts" do
-  mode 0755
+  mode 0750
   owner 'realntwk'
   group 'real'
   recursive true
@@ -9,11 +9,15 @@ end
 cookbook_file "/home/realntwk/deployment-scripts/deploy-requestserver.sh" do
   source "deploy-requestserver.sh"
   mode 0744
+  owner 'realntwk'
+  group 'real'
   action :create_if_missing
 end
 
 cookbook_file "/home/realntwk/deployment-scripts/deploy-jenkins.sh" do
   source "deploy-jenkins.sh"
   mode 0744
+  owner 'realntwk'
+  group 'real'
   action :create_if_missing
 end
