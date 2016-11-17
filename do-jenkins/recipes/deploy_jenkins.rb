f3bf0fa49e77156778d13e6bf646bb54ce67f367
Luke Luo <lukel@realnetworks.com>
2016-11-17 16:31:47 -0500

    fix ???

--- do-jenkins/recipes/deploy_jenkins.rb
+++ do-jenkins/recipes/deploy_jenkins.rb
@@ -2,6 +2,9 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   opsworks_deploy do
+    user 'realntwk'
+    group 'real'
+    path '/home/realntwk/tmp/'
     deploy_data deploy
     app application
   end
