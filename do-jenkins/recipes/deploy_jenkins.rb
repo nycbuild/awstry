2e490ab9fe3bf1cdcf3b92c7dbeba8e017df1c38
Luke Luo <lukel@realnetworks.com>
2016-11-18 09:25:50 -0500

    try1

--- do-jenkins/recipes/deploy_jenkins.rb
+++ do-jenkins/recipes/deploy_jenkins.rb
@@ -7,6 +7,16 @@
 
 
 include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+	deploy 'jenkins' do
+  		user 'realntwk'
+  		deploy_to '/home/realntwk/tmp'
+  		action :deploy
+	end
+end
+
+
 #
 #node[:deploy].each do |application, deploy|
 #  opsworks_deploy do
