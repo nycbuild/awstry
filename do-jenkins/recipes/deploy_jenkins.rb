43412eb2c4e5614cfa891c533463f76171d9a6eb
Luke Luo <lukel@realnetworks.com>
2016-11-19 17:56:24 -0500

    simplify

--- do-jenkins/recipes/deploy_jenkins.rb
+++ do-jenkins/recipes/deploy_jenkins.rb
@@ -1,31 +1,6 @@
-#deploy 'jenkins' do
-#  repo 'git@github.com:acctname/private-repo.git'
-#  user 'realntwk'
-#  deploy_to '/home/realntwk/tmp'
-#  action :deploy
-#end
-
-
-#include_recipe 'deploy'
-
 node[:deploy].each do |application, deploy|
   opsworks_deploy do
     deploy_data deploy
     app application
   end
 end
-
-#execute "deploy-post-jenkins" do
-#  user "realntwk"
-#  cwd "/home/realntwk/deployment-scripts"
-#  command "./deploy-post-jenkins.sh"
-#end
-
-
-#
-#node[:deploy].each do |application, deploy|
-#  opsworks_deploy do
-#    deploy_data deploy
-#    app application
-#  end
-#end
