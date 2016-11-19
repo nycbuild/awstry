8e9cc3afb9ebbee6e3169000b6edd69bacc3d7b5
Luke Luo <lukel@realnetworks.com>
2016-11-18 22:24:39 -0500

    separate

--- do-requestserver/recipes/default.rb
+++ do-requestserver/recipes/default.rb
@@ -1,5 +1,8 @@
-execute "do-requestserver" do
-  user "realntwk"
-  cwd "/home/realntwk/deployment-scripts"
-  command "./deploy-requestserver.sh"
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
 end
--- do-requestserver/recipes/deploy_requestserver.rb
+++ do-requestserver/recipes/deploy_requestserver.rb
@@ -1,12 +1,3 @@
-include_recipe 'deploy'
-
-node[:deploy].each do |application, deploy|
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
-end
-
 execute "deploy-post-requestserver" do
   user "realntwk"
   cwd "/home/realntwk/deployment-scripts"
