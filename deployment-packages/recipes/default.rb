c1950f5806d7c488aaed4fd6205bc8339d5df570
Luke Luo <lukel@realnetworks.com>
2016-11-18 19:01:29 -0500

    fix

--- deployment-packages/recipes/default.rb
+++ deployment-packages/recipes/default.rb
@@ -13,9 +13,4 @@ cookbook_file "/home/realntwk/deployment-packages/jenkins.war" do
   action :create_if_missing
 end
 
-cookbook_file "/home/realntwk/deployment-packages/sms.war" do
-  source "https://s3.amazonaws.com/nycbuild/requestserver/sms.war"
-  mode 0644
-  action :create_if_missing
-end
 
