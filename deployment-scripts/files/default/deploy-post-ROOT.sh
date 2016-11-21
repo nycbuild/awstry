f10e2a0374b14cceb5e74b2d3ef4d17ec07cdc04
Luke Luo <lukel@realnetworks.com>
2016-11-21 16:43:19 -0500

    remove site folder before deploy

--- deployment-scripts/files/default/deploy-post-ROOT.sh
+++ deployment-scripts/files/default/deploy-post-ROOT.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-rm -f /home/realntwk/tomcat/webapps/ROOT
+./shutdown.sh
+
+rm -rf /home/realntwk/tomcat/webapps/ROOT
 
 ln -s /srv/www/ROOT/current /home/realntwk/tomcat/webapps/ROOT
 
 cd /home/realntwk/tomcat/bin
 
-./shutdown.sh
 ./startup.sh
 
--- deployment-scripts/files/default/deploy-post-sms.sh
+++ deployment-scripts/files/default/deploy-post-sms.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-rm -f /home/realntwk/tomcat/webapps/sms
+./shutdown.sh
+
+rm -rf /home/realntwk/tomcat/webapps/sms
 
 ln -s /srv/www/sms/current /home/realntwk/tomcat/webapps/sms
 
 cd /home/realntwk/tomcat/bin
 
-./shutdown.sh
 ./startup.sh
 
