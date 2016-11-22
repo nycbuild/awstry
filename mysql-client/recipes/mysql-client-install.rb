31ca239dea62e465a9f6573c03e94cf4dd555e9f
Luke Luo <lukel@realnetworks.com>
2016-11-22 11:08:32 -0500

    add mysql-client

---
+++ mysql-client/recipes/mysql-client-install.rb
@@ -0,0 +1,14 @@
+package 'mysql-devel' do
+  package_name value_for_platform(
+    ['centos','redhat','fedora','amazon'] => {'default' => 'mysql-devel'},
+    'ubuntu' => {'default' => 'libmysqlclient-dev'}
+  )
+  action :install
+end
+package 'mysql-client' do
+  package_name value_for_platform(
+    ['centos','redhat','fedora','amazon'] => {'default' => 'mysql'},
+    'default' => 'mysql-client'
+  )
+  action :install
+end
