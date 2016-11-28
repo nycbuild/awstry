0a3e5e67c96138a7c4455f700b144e19de2b0fa0
Luke Luo <lukel@realnetworks.com>
2016-11-28 12:55:57 -0500

    remove ROOT for request server

--- deployment-scripts/files/default/scripts/requestserver/deploy-post-requestserver.sh
+++ deployment-scripts/files/default/scripts/requestserver/deploy-post-requestserver.sh
@@ -4,6 +4,7 @@
 cd /home/realntwk/tomcat/bin
 ./shutdown.sh
 
+rm -rf /home/realntwk/tomcat/webapps/ROOT
 rm -rf /home/realntwk/tomcat/webapps/sms
 
 ln -s /srv/www/requestserver/current /home/realntwk/tomcat/webapps/sms
